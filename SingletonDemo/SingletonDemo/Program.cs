266f04f1e5b0daf6ab4be9359a3ce90d503abc62
marcel-vasconcellos <59649432+marcel-vasconcellos@users.noreply.github.com>
2022-04-15 18:43:00 +0100

    Add files via upload

---
+++ SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Singleton Pattern Demonstration:");
+            Singleton1 firstInstance = Singleton1.GetInstance;
+            Singleton1 secondInstance = Singleton1.GetInstance;
+
+            if (firstInstance.Equals(secondInstance))
+            {
+                Console.WriteLine("The first and second instances are the same");
+            }
+            else
+            {
+                Console.WriteLine("The instances are different");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ SingletonDemo/SingletonDemo/Singleton1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton1
+    {
+        // private constructor is used to prevent the creation of instances with the new keyword outside of this class
+        private Singleton1() 
+        {
+            Console.WriteLine("Private constructor is called.");
+            Console.WriteLine("Exit private constructor.");
+        }
+
+        static Singleton1() 
+        {
+            Console.WriteLine("Static constructor is called.");
+            instance = new Singleton1();
+            TotalInstances++;
+            Console.WriteLine("Singleton instance is created. Numer of instances: {0}.", TotalInstances);
+            Console.WriteLine("Exiting static constructor.");
+        }
+        private static Singleton1 instance = null;
+        private static int TotalInstances;
+        public static Singleton1 GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton1();
+                }
+                return instance;
+            }
+        }
+    }
+}
