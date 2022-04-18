db2f10333defe267ab832eb256d02f4bbf60228c
marcel-vasconcellos <59649432+marcel-vasconcellos@users.noreply.github.com>
2022-04-18 23:07:21 +0100

    Add files via upload
    Demonstration of Prototype Pattern.

---
+++ PrototypePattern/ConcretePrototype.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    internal class ConcretePrototype : Prototype
+    {
+        public ConcretePrototype(string id, string info) : base(id, info)
+        {
+            Console.WriteLine("This is Concrete Prototype #{0} being created. The info is: {1}", id, info);
+        }
+        // returns a shallow copy
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+
+        // returns a shallow copy
+        public Prototype DeepClone()
+        {
+            Prototype clone = (Prototype)this.MemberwiseClone();
+            return clone;
+        }
+    }
+}
