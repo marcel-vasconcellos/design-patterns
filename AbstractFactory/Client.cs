39b2e8fc36a9ac60a5a1d055670a12cf7cbbe006
marcel-vasconcellos <59649432+marcel-vasconcellos@users.noreply.github.com>
2022-05-01 22:16:49 +0100

    Add files via upload

---
+++ AbstractFactory/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            // Abstract factory pattern is basically a wrapper around the constructor of an object
+            IAnimalFactory animalFactory = FactoryProvider.GetAnimalFactory("wild");
+            IDog dog = animalFactory.GetDog();
+            ICat cat = animalFactory.GetCat();
+            dog.AboutMe();
+            cat.AboutMe();
+            Console.WriteLine("*************");
+            animalFactory = FactoryProvider.GetAnimalFactory("pet");
+            dog = animalFactory.GetDog();
+            cat = animalFactory.GetCat();
+            dog.AboutMe();
+            cat.AboutMe();
+            Console.ReadKey();
+        }
+    }
+}
