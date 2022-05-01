26d03e0cec46438028f7e1368e1fba619b16f3d7
marcel-vasconcellos <59649432+marcel-vasconcellos@users.noreply.github.com>
2022-05-01 21:02:15 +0100

    Add files via upload

---
+++ SimpleFactoryPattern/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleFactoryPattern
+{
+    public class Client
+    {
+        static void Main(string[] args)
+        {
+            // Simple factory pattern is basically a wrapper around the constructor of an object
+            Console.WriteLine("Simple Factory Pattern demonstration!");
+            IAnimal preferredAnimal = null;
+            SimpleFactory simpleFactory = new SimpleFactory();
+            preferredAnimal = simpleFactory.CreateAnimal();
+            preferredAnimal.AboutMe();
+            Console.ReadKey();
+        }
+    }
+}
