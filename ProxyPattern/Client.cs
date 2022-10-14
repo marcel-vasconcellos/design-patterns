35898b51c2f15e12bd357ccda8e048f10ae3fd9a
marcel-vasconcellos <59649432+marcel-vasconcellos@users.noreply.github.com>
2022-10-14 13:07:05 +0100

    Add files via upload

---
+++ ProxyPattern/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            /*The objective of the proxy pattern is to be a placeholder fon an original object,
+              which access to has to be restricted. */
+            Console.WriteLine("***Proxy Pattern Demo***\n");
+            Subject proxy = new Proxy();
+            proxy.DoSomeWork();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProxyPattern/Proxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    public class Proxy : Subject
+    {   
+        // Proxy class will handle instantiation and the actual call to the original method.
+        Subject subject;
+        public override void DoSomeWork()
+        {
+            Console.WriteLine("Welcome, user.");
+            /* Lazy instantiation, we won't instantiate a new object until the method is called */
+            if(subject == null)
+            {
+                subject = new ConcreteSubject();
+            }
+            subject.DoSomeWork();
+        }
+    }
+}
